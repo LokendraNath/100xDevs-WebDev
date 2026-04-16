67a19d8b1c9766798ebfd1b3f07f874a8c82dfa8
Lokendra Nath <nathdeveloper25@gmail.com>
2026-04-16 21:49:58 +0530

    create simple dialog component

--- 16.1-websocket/02-BroadCast-ChatApp/client/src/App.tsx
+++ 16.1-websocket/02-BroadCast-ChatApp/client/src/App.tsx
@@ -1,4 +1,34 @@
+import { PlusSquare, Search } from "lucide-react";
+import { useState } from "react";
+import JoinGroupDialog from "./components/JoinGroupDialog";
+import CreateGroupDialog from "./components/CreateGroupDialog";
+
 const App = () => {
-  return <div>App</div>;
+  const [isJoinGroupDialogOpen, setIsJoinGroupOpen] = useState<boolean>(false);
+  const [isCreateGroupDialogOpen, setIsCreateGroupDialogOpen] =
+    useState<boolean>(false);
+  return (
+    <div className="relative flex gap-3 p-10 min-h-screen justify-center items-center">
+      <button
+        className="flex flex-col justify-center items-center cursor-pointer border p-2 rounded-2xl"
+        onClick={() => setIsCreateGroupDialogOpen(true)}
+      >
+        <PlusSquare /> Create Group
+      </button>
+      <button
+        className="flex flex-col justify-center items-center cursor-pointer border p-2 rounded-2xl"
+        onClick={() => setIsJoinGroupOpen(true)}
+      >
+        <Search /> Join Group
+      </button>
+
+      {isJoinGroupDialogOpen && (
+        <JoinGroupDialog setClose={() => setIsJoinGroupOpen(false)} />
+      )}
+      {isCreateGroupDialogOpen && (
+        <CreateGroupDialog setClose={() => setIsCreateGroupDialogOpen(false)} />
+      )}
+    </div>
+  );
 };
 export default App;
---
+++ 16.1-websocket/02-BroadCast-ChatApp/client/src/components/CreateGroupDialog.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+interface CreateGroupDialogProps {
+  setClose: () => void;
+}
+
+const CreateGroupDialog = ({ setClose }: CreateGroupDialogProps) => {
+  const [groupId, setGroupId] = useState("");
+  return (
+    <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
+      <div className="bg-white rounded-lg p-6 w-full max-w-md shadow-xl">
+        <h1>Create Group</h1>
+        <input
+          type="text"
+          onChange={(e) => setGroupId(e.target.value)}
+          className="border"
+        />
+        <button
+          onClick={() => setClose()}
+          className="border p-2 bg-red-500 text-white"
+        >
+          close
+        </button>
+        <button
+          onClick={() => setClose()}
+          className="border p-2 bg-green-500 text-white"
+          disabled={groupId.length <= 0}
+        >
+          Create
+        </button>
+      </div>
+    </div>
+  );
+};
+export default CreateGroupDialog;
