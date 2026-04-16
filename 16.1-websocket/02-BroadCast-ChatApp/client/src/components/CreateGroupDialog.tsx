84e71f57496e53eaf4a2a1b7b4efe9d1824c1879
Lokendra Nath <nathdeveloper25@gmail.com>
2026-04-16 22:39:59 +0530

    intall axios and create handle create group function

--- 16.1-websocket/02-BroadCast-ChatApp/client/src/components/CreateGroupDialog.tsx
+++ 16.1-websocket/02-BroadCast-ChatApp/client/src/components/CreateGroupDialog.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState } from "react";
 
 interface CreateGroupDialogProps {
@@ -5,14 +6,40 @@ interface CreateGroupDialogProps {
 }
 
 const CreateGroupDialog = ({ setClose }: CreateGroupDialogProps) => {
-  const [groupId, setGroupId] = useState("");
+  const [groupId, setGroupId] = useState<string | "">("");
+  const [groupName, setGroupName] = useState<string | "">("");
+
+  async function handleCreateGroup() {
+    if (!groupId && !groupName)
+      return alert("groupid or group name is invalid");
+
+    try {
+      const response = await axios.post("http://localhost:8080/createGroup", {
+        groupId,
+        groupName,
+      });
+      console.log("Group Created", response.data);
+      return response.data;
+    } catch (error) {
+      console.error("Server Error", error);
+    }
+  }
   return (
     <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
       <div className="bg-white rounded-lg p-6 w-full max-w-md shadow-xl">
         <h1>Create Group</h1>
         <input
-          type="text"
+          type="number"
+          placeholder="Enter Group Id"
           onChange={(e) => setGroupId(e.target.value)}
+          value={groupId}
+          className="border"
+        />
+        <input
+          type="text"
+          placeholder="Enter Group Name"
+          onChange={(e) => setGroupName(e.target.value)}
+          value={groupName}
           className="border"
         />
         <button
@@ -22,7 +49,7 @@ const CreateGroupDialog = ({ setClose }: CreateGroupDialogProps) => {
           close
         </button>
         <button
-          onClick={() => setClose()}
+          onClick={() => handleCreateGroup()}
           className="border p-2 bg-green-500 text-white"
           disabled={groupId.length <= 0}
         >
